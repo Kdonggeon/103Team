9cb0723c54eb74f1e63789c0390dc50ed90c5ce4
kkisim <163362240+kkisim@users.noreply.github.com>
2025-05-20 12:08:12 +0900

    아이디 비번 찾기 만드는중

---
+++ app/src/main/java/com/mobile/greenacademypartner/ui/FindAccountActivity.java
@@ -0,0 +1,15 @@
+package com.mobile.greenacademypartner.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mobile.greenacademypartner.R;
+
+public class FindAccountActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_account);
+    }
+}
--- app/src/main/java/com/mobile/greenacademypartner/ui/LoginActivity.java
+++ app/src/main/java/com/mobile/greenacademypartner/ui/LoginActivity.java
@@ -9,16 +9,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.mobile.greenacademypartner.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView findAccount = findViewById(R.id.find_account);
+    TextView signupText = findViewById(R.id.signup_next);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login); // ← 여기에 XML 파일 이름 넣기
 
-        TextView signupText = findViewById(R.id.signup_next);
+
 
         signupText.setOnClickListener(v -> {
             Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);
+        });
+        findAccount.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, FindAccountActivity.class);
             startActivity(intent);
         });
     }
