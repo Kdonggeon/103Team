99a18402547d9e36284a91626387314d6d5b40f2
vikovi <vikovi1108@gmail.com>
2025-11-15 17:13:53 +0900

    좌석 qr 오류 수정

--- mobile/app/src/main/java/com/mobile/greenacademypartner/api/RetrofitClient.java
+++ mobile/app/src/main/java/com/mobile/greenacademypartner/api/RetrofitClient.java
@@ -17,7 +17,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://192.168.0.23:9090/";
+    private static final String BASE_URL = "http://192.168.75.65:9090/";
 
 
     private static volatile Retrofit retrofit;
--- mobile/app/src/main/java/com/mobile/greenacademypartner/api/RoomApi.java
+++ mobile/app/src/main/java/com/mobile/greenacademypartner/api/RoomApi.java
@@ -7,9 +7,12 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface RoomApi {
+
+
     @PUT("/api/rooms/{roomNumber}/check-in")
     Call<ResponseBody> checkIn(
             @Path("roomNumber") int roomNumber,
+            @Query("academyNumber") int academyNumber,
             @Query("seatNumber") int seatNumber,
             @Query("studentId") String studentId
     );
